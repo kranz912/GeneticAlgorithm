62a06c63137b51dc1f390ae48478d7b46fe285ed
Kranz <=me@kranznikolai.com>
2019-12-12 23:24:53 -0500

    update test

--- Tictactoeplayer.py
+++ Tictactoeplayer.py
@@ -1,7 +1,13 @@
-from Individual import Individual 
+from Individual import Individual
 import random
+
+
+genes  = [0,1,2,3,4,5,6,7,8]
+population_size = 10
+
+
 class Player(Individual):
-    
+
     def __init__(self,chromosome):
         self.chromosome = chromosome
         self.fitness = self.Fitness()
@@ -15,6 +21,26 @@ class Player(Individual):
                 gene =self.mutate_genes(genes)
             genome.append(gene)
         return genome
+
+    def reproduce(self,partner):
+        child_chromosome = []
+        global genes
+        gene = self.mutate_genes(genes)
+        for gp1, gp2 in zip(self.chromosome, partner.chromosome):
+
+            while(gene in child_chromosome):
+                prob = random.random()
+                if prob < 0.45:
+                    gene=gp1
+                elif prob < 0.90:
+                    gene=gp2
+                else:
+                    gene = self.mutate_genes(genes)
+                    while gene in child_chromosome:
+                        gene = self.mutate_genes(genes)
+
+            child_chromosome.append(gene)
+        return Player(child_chromosome)
 
     def Fitness(self):
         board = [0,0,0,0,0,0,0,0,0]
@@ -48,20 +74,41 @@ class Player(Individual):
         return 0
 
 
-genes  = [0,1,2,3,4,5,6,7,8]
+
 players = []
-for x in range(10):
+for x in range(population_size):
     genome = Player.create_gnome(genes=genes,length=9)
     players.append(Player(genome))
 
 
 
-
-
-    
-
 #fitness([4, 7, 6, 0, 3, 1, 5, 2, 8])
+for _ in range(10000):
+
+    players = sorted(players, key= lambda x:x.fitness)
+
+    print('Generation: {}'.format(_+1))
+    for x in players:
+        winner = 'draw'
+        if x.fitness == 3:
+            winner = 'X'
+        elif x.fitness == -3:
+            winner = 'O'
+        print("moves: {}  winner: {}".format(x.chromosome,winner))
 
 
-for x in players:
-    print(x.fitness)
+    new_generation = []
+    elite_count =  int((10*population_size)/100)
+
+    new_generation.extend(players[:elite_count])
+
+    offspring_count= int((90*population_size)/100)
+
+    for __ in range(offspring_count):
+        parent1 = random.choice(players[:50])
+        parent2 = random.choice(players[:50])
+
+        child = parent1.reproduce(parent2)
+
+        new_generation.append(child)
+    players = new_generation
